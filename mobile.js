9a3b63dca84f0041930fd930a8aefba06fdcf357
Chriss <chriss713@gmail.com>
2021-09-26 11:23:50 +0200

    Finished chapter 10

--- mobile.js
+++ mobile.js
@@ -7,5 +7,18 @@ const iPhone = devices['iPhone 11'];
     // mobile code
     const browser = await chromium.launch({headless: false, slowMo: 300});
 
+    const context = await browser.newContext({
+        ...iPhone, // spread operator unpacks iPhone const
+        permissions: ['geolocation'], // allows geolocation
+        geolocation: {latitude: 19.432608, longitude: -99.133209}, // Mexico City coordinates
+        locale: 'fr-FR' // French from France
+    });
+
+    const page = await context.newPage();
+
+    await page.goto('https://www.google.com/maps');
+    // await page.click('text=J\'accepte');
+
+    await page.waitForTimeout(5000); // timeout of 5000ms only for debugging purposes
     await browser.close();
 })();
